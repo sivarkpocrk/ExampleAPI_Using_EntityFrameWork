75af7fd917da490e91f185b30d10e138cf632756
sivarktest2411 <92651216+sivarkpocrk@users.noreply.github.com>
2024-09-11 14:22:18 +0100

    Update AuthorDTO.cs

--- Models/AuthorDTO.cs
+++ Models/AuthorDTO.cs
@@ -1,1 +1,8 @@
+public class AuthorDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
 
