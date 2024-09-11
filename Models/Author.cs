f9cba688c8701971fbe332f71f5ed60d0e644f56
sivarktest2411 <92651216+sivarkpocrk@users.noreply.github.com>
2024-09-11 14:22:45 +0100

    Update Author.cs

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,1 +1,9 @@
+public class Author
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
 
