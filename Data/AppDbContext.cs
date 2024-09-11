4370c9865673282a4c14dbdb4c57f8a79944ba5a
sivarktest2411 <92651216+sivarkpocrk@users.noreply.github.com>
2024-09-11 14:23:56 +0100

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,11 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using CustomApi.Models;
 
 namespace CustomApi.Data
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        public DbSet<Product> Products { get; set; }
-    }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Author> Authors { get; set; } // Add this line for the Author table.
 }
